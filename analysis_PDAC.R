f89bf757146a7a32081e6b181368d2daf3573c60
Fraboulet Rose-Marie <fraboulet.rosemarie@gmail.com>
2020-06-30 10:52:17 +0200

    update

--- analysis_PDAC.R
+++ analysis_PDAC.R
@@ -19,6 +19,29 @@
 # write.table(metadata_pool, file="~/Documents/projet_stage/data/analysis/pool_metadata.tsv", sep="\t", row.names=T, col.names=T, quote=F)
 
 load("~/Documents/projet_stage/data/analysis/analysis.RData")
+load("/home/rose/Documents//projet_stage/data/data_clusters.RData")
+library(ggplot2)
+
+#Analysis pool
+head(metadata_pool)
+str(metadata_pool)         
+summary(metadata_pool)
+metadata_pool[, 4:6] <- as.numeric(unlist(metadata_pool[,4:6]))
+metadata_pool$cell <- as.factor(metadata_pool$cell)
+metadata_pool$cluster <- as.factor(metadata_pool$cluster)
+metadata_pool$orig.ident <- as.factor(metadata_pool$orig.ident)
+str(metadata_pool)
+
+#Analysis pool2
+head(metadata_pool2)
+str(metadata_pool2)         
+summary(metadata_pool2)
+metadata_pool2[, 3:4] <- as.numeric(unlist(metadata_pool2[,3:4]))
+metadata_pool2$cell <- as.factor(metadata_pool2$cell)
+metadata_pool2$seurat_clusters <- as.factor(metadata_pool2$seurat_clusters)
+metadata_pool2$orig.ident <- as.factor(metadata_pool2$orig.ident)
+str(metadata_pool2)   
+
+hist(metadata_pool2[,4], xlab="number of counts")
 
 
-         
