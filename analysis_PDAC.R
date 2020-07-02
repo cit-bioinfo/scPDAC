57658779cb09aaf709a63bd234d649137600c600
Fraboulet Rose-Marie <fraboulet.rosemarie@gmail.com>
2020-07-02 10:36:09 +0200

    data.frame treatment

--- analysis_PDAC.R
+++ analysis_PDAC.R
@@ -3,7 +3,7 @@ load("~/Documents/projet_stage/data/data/pool_object.RData")
 load("~/Documents/projet_stage/data/analysis_pool_object/metadata_pool_object.RData")
 
 #Data treatment 
-#Add metadat
+#Add metadata
 #metadata_pool_object <- read.table("metadata_pool_object.tsv", sep="\t", header=F, fill=T)
 #metadata_pool_object <- metadata_pool_object[-1,]
 #colnames(metadata_pool_object) <- c("cell", "orig_ident", "nCount_RNA", "nFeature_RNA", "percent_mt", "RNA_snn_res_1", "seurat_clusters", "peng_all_annotations")
@@ -22,17 +22,22 @@ load("~/Documents/projet_stage/data/analysis_pool_object/metadata_pool_object.RD
 #active_ident_pool_object_orig_ident <- read.table("active_ident_pool_object_orig_ident.tsv", sep="\t", header=F, fill=T)
 #active_ident_pool_object_orig_ident <- active_ident_pool_object_orig_ident[-1,]
 #colnames(active_ident_pool_object_orig_ident) <- c("cell", "orig_ident_N/T")
+#colnames(metadata_pool_object)[which(names(metadata_pool_object)=="orig_ident_N/T")] <- "orig_ident_N_T"
 #metadata_pool_object <- merge(metadata_pool_object, active_ident_pool_object_orig_ident, by.group="cell")
 #metadata_pool_object <- metadata_pool_object[, c(1, 2, 3, 4, 5, 9, 6, 7, 8)]
 #write.table(metadata_pool_object, file="~/Documents/projet_stage/data/analysis_pool_object/pool_metadata_object.tsv", sep="\t", row.names=F, col.names=T, quote=F)
 
+##
+# head(metadata_pool_object)
+# str(metadata_pool_object) 
+# summary(metadata_pool)
+# metadata_pool_object[, 2:4] <- as.numeric(unlist(metadata_pool_object[,2:4]))
+# metadata_pool_object$cell <- as.factor(metadata_pool_object$cell)
+# metadata_pool_object$orig_ident <- as.factor(metadata_pool_object$orig_ident)
+# metadata_pool_object$orig_ident_N_T <- as.factor(metadata_pool_object$orig_ident_N_T)
+# metadata_pool_object$seurat_clusters <- as.factor(metadata_pool_object$seurat_clusters)
+# metadata_pool_object$my_annotations <- as.factor(metadata_pool_object$my_annotations)
+# metadata_pool_object$peng_all_annotations <- as.factor(metadata_pool_object$peng_all_annotations)
+# str(metadata_pool_object)
 
-#Analysis 
-head(metadata_pool)
-str(metadata_pool)         
-summary(metadata_pool)
-metadata_pool[, 4:6] <- as.numeric(unlist(metadata_pool[,4:6]))
-metadata_pool$cell <- as.factor(metadata_pool$cell)
-metadata_pool$cluster <- as.factor(metadata_pool$cluster)
-metadata_pool$orig.ident <- as.factor(metadata_pool$orig.ident)
-str(metadata_pool)
+
