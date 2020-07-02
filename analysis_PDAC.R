063969dbd67e092e0c5ee9145e29104e36fc0f8d
Fraboulet Rose-Marie <fraboulet.rosemarie@gmail.com>
2020-07-02 10:11:33 +0200

    data treatment

--- analysis_PDAC.R
+++ analysis_PDAC.R
@@ -1,19 +1,31 @@
 #Loading dataset 
+load("~/Documents/projet_stage/data/data/pool_object.RData")
+load("~/Documents/projet_stage/data/analysis_pool_object/metadata_pool_object.RData")
 
-load("~/Documents/projet_stage/data/analysis/metadata_pool_object.RData")
-
+#Data treatment 
+#Add metadat
 #metadata_pool_object <- read.table("metadata_pool_object.tsv", sep="\t", header=F, fill=T)
 #metadata_pool_object <- metadata_pool_object[-1,]
 #colnames(metadata_pool_object) <- c("cell", "orig_ident", "nCount_RNA", "nFeature_RNA", "percent_mt", "RNA_snn_res_1", "seurat_clusters", "peng_all_annotations")
 #head(metadata_pool)
 
+##Add ident
 #active_ident_pool_object <- read.table("active_ident_pool_object.tsv", sep="\t", header=F, fill=T)
 #active_ident_pool_object <- active_ident_pool_object[-1,]
 #colnames(active_ident_pool_object) <- c("cell", "my_annotations")
 #metadata_pool_object <- merge(active_ident_pool_object, metadata_pool_object, by="cell")
 #metadata_pool_object$RNA_snn_res_1 <- NULL
 #metadata_pool_object <- metadata_pool_object[, c(1, 4, 5, 6, 3, 7, 2, 8)]
-#write.table(metadata_pool_object, file="~/Documents/projet_stage/data/analysis/pool_metadata_object.tsv", sep="\t", row.names=F, col.names=T, quote=F)
+#write.table(metadata_pool_object, file="~/Documents/projet_stage/data/analysis_pool_object/pool_metadata_object.tsv", sep="\t", row.names=F, col.names=T, quote=F)
+
+##Add N/T orig ident
+#active_ident_pool_object_orig_ident <- read.table("active_ident_pool_object_orig_ident.tsv", sep="\t", header=F, fill=T)
+#active_ident_pool_object_orig_ident <- active_ident_pool_object_orig_ident[-1,]
+#colnames(active_ident_pool_object_orig_ident) <- c("cell", "orig_ident_N/T")
+#metadata_pool_object <- merge(metadata_pool_object, active_ident_pool_object_orig_ident, by.group="cell")
+#metadata_pool_object <- metadata_pool_object[, c(1, 2, 3, 4, 5, 9, 6, 7, 8)]
+#write.table(metadata_pool_object, file="~/Documents/projet_stage/data/analysis_pool_object/pool_metadata_object.tsv", sep="\t", row.names=F, col.names=T, quote=F)
+
 
 #Analysis 
 head(metadata_pool)
